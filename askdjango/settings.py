ea05397ba681f1ea4ec3d413726f530334ed1c2d
osandra <okhw97@gmail.com>
2020-07-26 15:30:42 +0900

    04 로그아웃 구현하기

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.urls import reverse_lazy
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -54,6 +56,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'askdjango.urls'
 
+
+# LOGOUT_REDIRECT_URL = reverse_lazy('login')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
